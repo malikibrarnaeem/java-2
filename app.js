e1c903552aeb560e0d51ecbaea6b2fe26acb41fc
malikibrarnaeem <95858450+malikibrarnaeem@users.noreply.github.com>
2021-12-13 16:53:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,141 @@
+// CH 21to25 start
+
+
+// var fname = prompt("enter your first name ")
+// var Lastname = prompt("enter your last name ")
+// var fullname = fname + Lastname + ("welcome to my website ")
+// alert(fullname)
+
+
+
+// var mobile = prompt("enter your favorite mobile model ")
+// for(i=0; i < mobile.length; i++){
+
+//    console.log(i)
+// }
+
+
+
+// var pak = ("pakistan")
+
+// for(i=0; i < pak.length; i++){
+//     var a = pak.slice(i,i + 1)
+//     if(a == "n")
+//     console.log("numbers of string are  " + i)
+// }
+
+
+// var ab = ("hello world")
+
+// for(i=0; i < ab.length; i++){
+//     var a = ab.splice(i,i + 1)
+//     if(a == "l")
+//     console.log("numbers of string are  " + i)
+// }
+
+
+// var pak = ("pakistan")
+
+// for(i=0; i < pak.length; i++){
+//     var a = pak.slice(i,i + 1)
+//     if(a == "i")
+//     console.log("numbers of string are  " + i)
+// }
+
+
+
+// var a = ("hyderabad")
+// var b = a.replace("hydera","islama")
+// alert(b)
+
+
+
+// var friend = "Ali and Sami are best friends. They play cricket and football together"
+// var b = friend.replace("and","&")
+// alert(b)
+
+
+
+
+// var ab = 476;
+// var ba = a.toString()
+// console.log(ba)
+
+
+
+// var ab = "hello world"
+// var ba = a.toUpperCase()
+// alert(ba)
+
+
+
+// var ab = prompt("Enter your comment")
+// var ba = a.toUpperCase()
+// alert(ba)
+
+
+// var ab = "35.34"
+// var ba = parseInt(ab)
+// console.log(ba)
+
+
+// var a = prompt("Enter username")
+// if(a == "!"){
+//     alert("this is invalid username")
+// }
+
+
+// var a = ["cake","pastry","chips"]   
+
+// for(i = 0; i < a.length; i++){
+//     var b = prompt("what you want")
+//     if(b = "cake" ){
+//         alert("available")
+//     }
+// }
+
+
+
+// CH 21to25 end
+
+// CH 25to30 start
+
+// var a = 3.54
+// var b = Math.round(a)
+// var c = Math.ceil(a)
+// var d = Math.floor(a)
+// alert(d)
+
+
+
+
+// var a = -3.54
+// var b = Math.round(a)
+// var c = Math.ceil(a)
+// var d = Math.floor(a)
+// alert(d)
+
+
+
+
+
+
+
+// var a = 1.5;
+// var b = Math.random(a);
+// var c = Math.round(b) 
+
+// if(c == 0){
+//     console.log("head")
+// }else(console.log("tails"))
+
+
+
+
+
+
+
+
+
+
+// CH 25to30 end
